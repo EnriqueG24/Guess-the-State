1ae84011ab745f01751f1f154d0dadca2ae45c63
Enrique Gongora <EGTech24@gmail.com>
2021-02-16 18:00:18 -0800

    Game successfully works. When a state is correctly guessed, the name appears over the state on the map

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.ht()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
