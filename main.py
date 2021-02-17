657ddf99489064d6d5cefa29b1315b315e91e305
Enrique Gongora <EGTech24@gmail.com>
2021-02-16 17:58:22 -0800

    Extracted the information from the csv file into a list using pandas

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import turtle
 import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
